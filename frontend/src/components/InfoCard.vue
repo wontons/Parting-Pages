96b143d0ff949c19d6ba86d99f3914d721c0f9e4
Anton Sawka <antonsawka2@gmail.com>
2019-12-25 16:02:52 -0600

    free use images

--- frontend/src/components/InfoCard.vue
+++ frontend/src/components/InfoCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-row sm:flex-col rounded-lg border shadow-lg w-1/2 max-w-xl sm:w-full bg-gray-200 m-4">
-    <img alt="Book" src="" class="flex w-32 h-32 sm:w-full"/>
+    <img alt="Book" src="Books" class="flex w-32 h-32 sm:w-full"/>
     <div class="flex flex-col bg-white m-2">
       <div class="text-black text-center font-bold m-2">Welcome to Parting Pages</div>
       <div class="text-black text-center m-2">A marketplace for used textbooks</div>
@@ -9,6 +9,8 @@
 </template>
 
 <script>
+import Books from "@/assets/books.jpg";
+
 export default {
     
 }
