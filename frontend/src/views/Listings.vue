b62eb39670bd09bafce9b790fff131328f6468cb
wontons <antonsawka@hotmail.com>
2019-12-30 07:57:41 -0600

    cleanup

--- frontend/src/views/Listings.vue
+++ frontend/src/views/Listings.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="mb-8">
-      <Booklist v-bind:books="books"/>
+      <Booklist />
     </div>
 </template>
 
@@ -11,42 +11,6 @@ export default {
   name: "listings",
   components: {
     Booklist
-  },
-  data () {
-    return {
-      books: [
-        {
-          id: 1,
-          title: "Cat in the Hat",
-          author: "Dr. Suess",
-          publisher: "cat publishing",
-          isbn: "1234-123123123123-123",
-          sold: false,
-          condition: "Fine",
-          highlighted: true
-        },
-        {
-          id: 2,
-          title: "Cat in the sock",
-          author: "Miss Suess",
-          publisher: "cat publishing",
-          isbn: "3333-123123123123-123",
-          sold: false,
-          condition: "Poor",
-          highlighted: false
-        },
-        {
-          id: 3,
-          title: "Cat in the dock",
-          author: "Mr. Suess",
-          publisher: "cat publishing",
-          isbn: "6666-123123123123-123",
-          sold: true,
-          condition: "Good",
-          highlighted: true
-        }
-      ]
-    }
   }
-}
+};
 </script>
